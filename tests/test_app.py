64c454c024f53492e8e7ca8242e951dc83902a72
CalcioDM3 <calciodm3@gmail.com>
2025-06-29 17:48:24 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
 import unittest
 from unittest.mock import MagicMock, patch
 from main import FootballApp
