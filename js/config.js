080d202ae514adbb6b4ea1e292cbb41b57cf489a
CalcioDM3 <calciodm3@gmail.com>
2025-09-05 12:25:05 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Questo file viene generato automaticamente da GitHub Actions
+// Non versionarlo nel repository!
+const GITHUB_TOKEN = '';
+const GITHUB_USER = 'CalcioDM3';
+const GITHUB_REPO = 'CalcioDM3';
