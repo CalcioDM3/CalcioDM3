135d56b53c0fa9a84b62d170d5a6a6c915309598
CalcioDM3 <calciodm3@gmail.com>
2025-09-08 17:05:32 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,19 +11,10 @@
     adminUsers: ['Gianmarco Saponaro', 'Marco D\'Amato']
   };
 
-  // Cerca il token nell'URL
-  function getTokenFromURL() {
-    try {
-      const urlParams = new URLSearchParams(window.location.search);
-      return urlParams.get('token');
-    } catch (e) {
-      console.error('Errore nel parsing dell URL:', e);
-      return '';
-    }
+  // Inizializza GITHUB_CONFIG se non esiste
+  if (typeof window.GITHUB_CONFIG === 'undefined') {
+    window.GITHUB_CONFIG = {};
   }
-
-  // Configurazione finale - con fallback sicuro
-  window.GITHUB_CONFIG = window.GITHUB_CONFIG || {};
   
   // Merge con la configurazione di default
   const configKeys = Object.keys(defaultConfig);
@@ -34,13 +25,7 @@
     }
   }
 
-  // Sovrascrivi il token se presente nell'URL
-  const urlToken = getTokenFromURL();
-  if (urlToken) {
-    window.GITHUB_CONFIG.token = urlToken;
-    console.log('Token impostato dall URL');
-  }
-
+  // Verifica finale
   console.log('Configurazione caricata:', {
     user: window.GITHUB_CONFIG.user,
     repo: window.GITHUB_CONFIG.repo,
